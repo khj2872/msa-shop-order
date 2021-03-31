d13fd2e6570a5b4b59f49e359b8a9592bf093a12
Hyeokjin <gurwls1060@naver.com>
2021-03-31 22:42:46 +0900

    first commit

---
+++ src/main/java/com/msa/shop/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.msa.shop.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
