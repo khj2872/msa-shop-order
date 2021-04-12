c3829650e03595a32e30e8441cfb0b8e8f5a706c
Hyeokjin <gurwls1060@naver.com>
2021-04-12 23:54:28 +0900

    EnableJpaAuditing 추가

--- src/main/java/com/msa/shop/order/OrderApplication.java
+++ src/main/java/com/msa/shop/order/OrderApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
 @EnableBinding(Source.class)
+@EnableJpaAuditing
 @SpringBootApplication
 public class OrderApplication {
 
